874b39ff4b1f405147dd0de2ad6db9fcf158e8ba
suhailtajshaik <suhailtajshaik@gmail.com>
2023-07-03 23:26:48 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import reactLogo from '/assets/react.svg'
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React</h1>
+    </>
+  )
+}
+
+export default App
